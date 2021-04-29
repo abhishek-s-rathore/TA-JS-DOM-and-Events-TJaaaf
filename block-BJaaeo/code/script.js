81c8b0746020e16cbfd558540b1d82b1b2a5d5e1
Abhishek <abhishek.990914@gmail.com>
2021-04-29 12:01:04 +0530

    Refectored

--- block-BJaaeo/code/script.js
+++ block-BJaaeo/code/script.js
@@ -1,19 +1,30 @@
-let btns = document.querySelectorAll(".box");
-let display = document.querySelector(".soln");
-let equal = document.querySelector(".equal");
-let clear = document.querySelector(".clear");
+function main() {
+  let btns = document.querySelectorAll(".box");
+  let display = document.querySelector(".soln");
+  let equal = document.querySelector(".equal");
+  let clear = document.querySelector(".clear");
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", (event) => {
-    let text = event.target.dataset.text;
-    display.innerText += event.target.dataset.text;
+  btns.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      let text = event.target.dataset.text;
+      display.innerText += event.target.dataset.text;
+      let bgclr = btn.style.backgroundColor;
+
+      btn.style.backgroundColor = "gray";
+      setTimeout(() => {
+        btn.style.backgroundColor = bgclr;
+      }, 250);
+    });
   });
-});
 
-clear.addEventListener("click", () => {
-  display.innerText = "";
-});
+  clear.addEventListener("click", () => {
+    display.innerText = "";
+  });
 
-equal.addEventListener("click", () => {
-  display.innerText = eval(display.innerText);
-});
+  equal.addEventListener("click", () => {
+    display.innerText = eval(display.innerText);
+  });
+  display.innerText = 0;
+}
+
+main();
