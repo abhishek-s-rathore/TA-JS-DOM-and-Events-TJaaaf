e3c1813366218b36c83c89b7e1a0090c83ffb7da
Abhishek <abhishek.990914@gmail.com>
2021-05-11 21:50:34 +0530

    refectored

--- block-BJaaet/code/script.js
+++ block-BJaaet/code/script.js
@@ -1,25 +1,50 @@
 let form = document.querySelector("form");
+let ul = document.querySelector("ul");
+let input = document.querySelector("input");
 let movies = [];
+
+function deleteMovie(event) {
+  let id = event.target.dataset.id;
+  movies.splice(id, 1);
+  event.target.parentElement.remove();
+}
 
 function handleSubmit(event) {
   event.preventDefault();
   let movie = event.target.elements.movieInput.value;
-  movies.push(movie);
+
+  // Create Array
+  movies.push({
+    name: movie,
+    watched: false,
+  });
   console.log(movies);
 
-  let div = document.createElement("div");
-  let checkbox = document.createElement("input");
-  let para = document.createElement("p");
-  let icon = document.createElement("i");
-  checkbox.style.marginRight = "1rem";
-  checkbox.setAttribute("type", "checkbox");
-  para.innerText = movie;
-  icon.style.color = "red";
-  icon.style.marginLeft = "2.5rem";
-  icon.classList.add("fas", "fa-times");
-  div.append(checkbox, para, icon);
-  div.classList.add("flex", "justify-start", "align-center");
-  form.append(div);
+  // Create UI
+  ul.innerHTML = "";
+  movies.forEach((movie, i) => {
+    let li = document.createElement("li");
+    let checkbox = document.createElement("input");
+    let label = document.createElement("label");
+    let icon = document.createElement("i");
+    checkbox.style.marginRight = "1rem";
+    checkbox.type = "checkbox";
+    checkbox.id = i;
+    label.for = i;
+    checkbox.checked = movies.watched;
+    label.innerText = movie.name;
+    icon.style.color = "red";
+    icon.style.marginLeft = "2.5rem";
+    icon.setAttribute("data-id", i);
+    icon.classList.add("fas", "fa-times");
+    li.append(checkbox, label, icon);
+    li.classList.add("flex", "justify-start", "align-center");
+    ul.append(li);
+
+    input.value = "";
+    // Deleting Movie
+    icon.addEventListener("click", deleteMovie);
+  });
 }
 
 form.addEventListener("submit", handleSubmit);
